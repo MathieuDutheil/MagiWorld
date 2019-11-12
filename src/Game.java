21ee0be330ec8879c2065e9ba4fbbd9a5e385bdd
Mathieu Dutheil <mathieudutheil92@gmail.com>
2019-11-12 11:52:50 +0100

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,24 @@
+public class Game {
+
+    public class Character {
+
+        int level;
+        int life=level*5;
+        int strength ;
+        int agility;
+        int intelligence;
+
+
+
+    }
+
+    public class
+
+
+
+
+
+
+
+
+}
