82f7f758e52e3167426aa8a7f4e63da768bf9b77
Mathieu Dutheil <mathieudutheil92@gmail.com>
2019-11-12 21:21:23 +0100

    delete game class create character class

---
+++ src/Character.java
@@ -0,0 +1,26 @@
+public class Character {
+
+        public int level;
+        public int life=level*5;
+        public int strength ;
+        public int agility;
+        public int intelligence;
+
+        public void basicAttack() {
+
+        }
+
+        public void specialAttack() {
+
+        }
+
+
+
+
+
+
+
+
+}
+
+
