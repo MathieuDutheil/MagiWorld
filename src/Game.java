890d966a6ddc2f27de574d7f44531a1567b5326e
Mathieu Dutheil <mathieudutheil92@gmail.com>
2019-11-20 17:21:20 +0100

    add for loop to createCharacters

--- src/Game.java
+++ src/Game.java
@@ -4,37 +4,37 @@ public class Game {
     Scanner sc = new Scanner(System.in);
 
     public void createCharacters() {
-        System.out.println("Création du personnage du joueur 1");
-        System.out.println("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 : Mage)");
-        int nbCharacter1 = sc.nextInt();
-        System.out.println("Niveau du personnage ?");
-        int levelCharacter1 = sc.nextInt();
-        int lifeCharacter1 = levelCharacter1*5;
-        System.out.println("Force du personnage ?");
-        int strenghtCharacter1 = sc.nextInt();
-        System.out.println("Agilité du personnage ?");
-        int agilityCharacter1 = sc.nextInt();
-        System.out.println("Intelligence du personnage ?");
-        int intelligenceCharacter1 = sc.nextInt();
+        for (int i=1; i<3; i++) {
+            System.out.println("Création du personnage du joueur " + i);
+            System.out.println("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 : Mage)");
+            int nbCharacter = sc.nextInt();
+            System.out.println("Niveau du personnage ?");
+            int levelCharacter = sc.nextInt();
+            int lifeCharacter = levelCharacter * 5;
+            System.out.println("Force du personnage ?");
+            int strenghtCharacter = sc.nextInt();
+            System.out.println("Agilité du personnage ?");
+            int agilityCharacter = sc.nextInt();
+            System.out.println("Intelligence du personnage ?");
+            int intelligenceCharacter = sc.nextInt();
 
-        switch (nbCharacter1) {
-            case 1:
-                System.out.println("Woarg je suis le Guerrier Joueur 1 niveau " + levelCharacter1 + " je possède " + lifeCharacter1 + " de vitalité, " + strenghtCharacter1 + " de force, " + agilityCharacter1 + " d'agilité et " + intelligenceCharacter1 + " d'intelligence !");
-                break;
+            switch (nbCharacter) {
+                case 1:
+                    System.out.println("Woarg je suis le Guerrier Joueur " + i + " niveau " + levelCharacter + " je possède " + lifeCharacter + " de vitalité, " + strenghtCharacter + " de force, " + agilityCharacter + " d'agilité et " + intelligenceCharacter + " d'intelligence !");
+                    break;
                 /* miss Character create */
-            case 2:
-                System.out.println("Mouahahaha je suis le Rôdeur Joueur 1 niveau " + levelCharacter1 + " je possède " + lifeCharacter1 + " de vitalité, " + strenghtCharacter1 + " de force, " + agilityCharacter1 + " d'agilité et " + intelligenceCharacter1 + " d'intelligence !");
-                break;
+                case 2:
+                    System.out.println("Mouahahaha je suis le Rôdeur Joueur " + i + " niveau " + levelCharacter + " je possède " + lifeCharacter + " de vitalité, " + strenghtCharacter + " de force, " + agilityCharacter + " d'agilité et " + intelligenceCharacter + " d'intelligence !");
+                    break;
                 /* miss Character create */
-            case 3:
-                System.out.println("Abracadabra je suis le Mage Joueur 1 niveau " + levelCharacter1 + " je possède " + lifeCharacter1 + " de vitalité, " + strenghtCharacter1 + " de force, " + agilityCharacter1 + " d'agilité et " + intelligenceCharacter1 + " d'intelligence !");
-                break;
-            default:
-                System.out.println("Vous n'avez pas choisi de classe parmi les choix proposés");
+                case 3:
+                    System.out.println("Abracadabra je suis le Mage Joueur " + i + " niveau " + levelCharacter + " je possède " + lifeCharacter + " de vitalité, " + strenghtCharacter + " de force, " + agilityCharacter + " d'agilité et " + intelligenceCharacter + " d'intelligence !");
+                    break;
+                /* miss Character create */
+                default:
+                    System.out.println("Vous n'avez pas choisi de classe parmi les choix proposés");
+            }
         }
-
-
-
     }
 
     public void runGame() {
